2ea4ad340b6266f7969a9b0cf05fdbd531495f84
Skencker <Sandrinekencker@hotmail.com>
2021-04-19 19:13:05 +0200

    modal-del

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*menu burger*/ 
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar-menu')
 
@@ -5,4 +6,24 @@ const menuLinks = document.querySelector('.navbar-menu')
 menu.addEventListener('click', function(){
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
+})
+
+/**modal */
+
+const btn = document.querySelector('.modal-btn');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
+const body = document.querySelector('body');
+const svg = document.querySelector('svg')
+
+btn.addEventListener('click', () => {
+  modal.style.display ='flex';
+  body.classList.toggle('masque');
+  svg.style.display = 'none';
+})
+
+close.addEventListener('click', () => {
+  modal.style.display = "none";
+  body.classList.toggle('masque');
+  svg.style.display = 'flex';
 })
